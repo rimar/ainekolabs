c213ba35a089ebccadc898b859501ab7996f5685
Yuval Rimar <yuval.rimar@gmail.com>
2009-01-26 02:24:11 +0200

    simpler client core

--- client.rb
+++ client.rb
@@ -1,63 +1,22 @@
-require 'remote'
-require 'gitty'
+require 'find'
+require 'date'
+require 'envs'
 
 class Client
+  def initialize() @shouldRun = true end
   def main
-    @remote = Remote.new
-    @remote.register
-    j = @remote.getJobs
-    for job in j do
-      JobStarter.new.startJob(job)
-    end
-    @remote.reportStatistics
-  end
-end
-
-class JobStarter
-  def startJob(job)
-    @all = {}
-    initAll(job)
-    introduceAll
-    startAll
-
-    puts "DEBUG: " + @all.to_s
-  end
-
-  def initAll(job)
-    # Load the bots and agents from their gits
-    for key in job.urls.keys
-      url = job.urls[key]
-      puts("url=" + url)
-      git = Gitty.new(url)
-      updated = git.update
-      require git.name
-      loaded = init()
-      @all[key] = loaded
-    end
-  end
-  def introduceAll
-    # Introduce between bots and agents if they want to
-    for key in @all.keys
-      bot = @all[key]
-      if (bot.respond_to?('introduce'))
-        for otherKey in @all.keys
-          if (key != otherKey)
-            bot.introduce(key, @all[key])
-          end
+    while (@shouldRun) do
+      Find.find(Envs.ainekodir) do |path|
+        next if path == Envs.ainekodir
+        if FileTest.directory?(path)
+          puts(path)
+          Find.prune
         end
       end
+      puts DateTime.now
+      sleep(2)
     end
   end
-  def startAll
-    for key in @all.keys
-      bot = @all[key]
-
-      if (bot.respond_to?('start'))
-        bot.start
-      end
-    end
-  end
-
 end
 
 if $0 == __FILE__
